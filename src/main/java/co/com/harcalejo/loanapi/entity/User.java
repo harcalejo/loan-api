0bfa5249306efd5761010218da11dcbe8ef5506f
Harcalejo <harcalejo@gmail.com>
2022-09-26 14:35:44 -0500

    Ajustes en la documentacion

--- src/main/java/co/com/harcalejo/loanapi/entity/User.java
+++ src/main/java/co/com/harcalejo/loanapi/entity/User.java
@@ -29,9 +29,8 @@ import javax.persistence.ManyToOne;
  * en tiempo de compilación y así mantener nuestro código simple.
  *
  * Setter - Anotación de la librería Lombok usada para definir que los atributos
- * de la clase deben tener asociado un método {@code Get}, para que sean generados
+ * de la clase deben tener asociado un método {@code Set}, para que sean generados
  * en tiempo de compilación y así mantener nuestro código simple.
- * clase tiene métodos
  *
  * @author Hugo Alejandro Rodriguez
  * @version 1.0.0
