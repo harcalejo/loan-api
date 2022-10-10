4c5bab3b81ec66ae26945ac5bf95558731b1367a
Harcalejo <harcalejo@gmail.com>
2022-10-10 17:49:42 -0500

    Incluyendo configuracion para usar Eureka como service discovery

--- src/main/java/co/com/harcalejo/loanapi/LoanApiApplication.java
+++ src/main/java/co/com/harcalejo/loanapi/LoanApiApplication.java
@@ -2,8 +2,10 @@ package co.com.harcalejo.loanapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LoanApiApplication {
 
 	public static void main(String[] args) {
